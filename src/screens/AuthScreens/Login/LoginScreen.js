2fa5d193375c7108efd1ad3cb96d1d47dee626b9
Ajaykumar Rajasekaran <ajaykumarrajasekaran@Ajaykumars-MacBook-Pro.local>
2023-03-02 11:28:27 +0530

    added signing configs and firebase keysotre for google singin puropose

--- src/screens/AuthScreens/Login/LoginScreen.js
+++ src/screens/AuthScreens/Login/LoginScreen.js
@@ -1,5 +1,34 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from '@react-native-google-signin/google-signin';
+import auth from '@react-native-firebase/auth'
+
+GoogleSignin.configure({
+  webClientId: '476622320202-lljc2aaj9sfnvh3ospstbrq58tm79hhf.apps.googleusercontent.com',
+});
+
+
+const signIn = async () => {
+  try {
+    await GoogleSignin.hasPlayServices();
+    const userInfo = await GoogleSignin.signIn();
+    this.setState({ userInfo });
+  } catch (error) {
+    if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+      // user cancelled the login flow
+    } else if (error.code === statusCodes.IN_PROGRESS) {
+      // operation (e.g. sign in) is in progress already
+    } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+      // play services not available or outdated
+    } else {
+      // some other error happened
+    }
+  }
+};
 
 const LoginScreen = () => {
   return (
